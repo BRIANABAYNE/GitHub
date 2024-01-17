4e825a7356845e91d483c647f9ede58f5d7adc48
Briana Bayne <brianabayne1@gmail.com>
2024-01-17 11:08:50 -0700

    Created a custom button that will be used throughout the app.

---
+++ GITHUB/GITHUB/GFButton.swift
@@ -0,0 +1,48 @@
+//
+//  GFButton.swift
+//  GITHUB
+//
+//  Created by Briana Bayne on 1/17/24.
+//
+
+import UIKit
+
+// creating a custom button that I will be using throughout the app
+
+// subclass of UIButton called GFButton
+class GFButton: UIButton {
+    
+    // override the init since I am going to a custom button
+    override init(frame: CGRect) {
+        // super means I am calling the parent class or super class - calling everything that a UIButton has and then doing the custom code
+        /// subclass - building onto of the existing objects
+        // Building our GFButton on top on UIButton
+        // GF Button is the child class of the UIButton that would be the parent class
+        super.init(frame: frame)
+        // calling the custom button that I created
+        configure()
+    }
+    
+    // This gets called when you initialize the button via storyboard - we aren't using storyboard , handles the init when using storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        // self means, this GF button
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        // calling our custom code
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        // using this for a dynamic type font, dynamic type is where the user can change the size of the font in their phone. If you don't use a dynamic type font, the user will not be able to change the font size. This is something that is built into the apple iphone.
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        // use autoLayout - always set to false
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
