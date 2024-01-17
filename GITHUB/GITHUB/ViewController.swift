65f0a464c030f0d14fce224753f0b7f9ad8f6c47
Briana Bayne <brianabayne1@gmail.com>
2024-01-16 20:40:46 -0700

    Deleted Storyboard file, add code for VC, added assets for project.

---
+++ GITHUB/GITHUB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GITHUB
+//
+//  Created by Briana Bayne on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
