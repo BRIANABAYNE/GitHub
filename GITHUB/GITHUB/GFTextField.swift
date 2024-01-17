56adf1f7362063b48385075d7a64e2d00bb050a8
Briana Bayne <brianabayne1@gmail.com>
2024-01-17 11:35:37 -0700

    Created TextField

---
+++ GITHUB/GITHUB/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField1.swift
+//  GITHUB
+//
+//  Created by Briana Bayne on 1/17/24.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    // storyboard init
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureText() {
+        // need this for the constrains, always set to false
+        translatesAutoresizingMaskIntoConstraints = false
+        // edge of the textField
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        // This is auto correct when a user is typing, this wouldn't be needed since we user will be typing in a name and we wouldn't want it to auto correct.
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+        
+    }
+}
