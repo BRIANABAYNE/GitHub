2f1f97217a296bab42ce2e92c4a0e53a5bcdc3bb
Briana Bayne <brianabayne1@gmail.com>
2024-01-17 20:28:13 -0700

    Addeded files for custom label and Alert.

---
+++ GITHUB/GITHUB/GFAlertVC.swift
@@ -0,0 +1,61 @@
+//
+//  GFAlertVC.swift
+//  GITHUB
+//
+//  Created by Briana Bayne on 1/17/24.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    let containerView = UIView()
+    let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GFBodyLabel(textAlignment: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "OKAY")
+
+    var alertTile: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    
+    // MARK: - Init
+    
+    init(alertTile: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTile = title
+        self.message = message
+        self.buttonTitle = buttonTitle
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        configureContainerView()
+    }
+    
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+
+     }
+}
---
+++ GITHUB/GITHUB/GFTitleLabel.swift
@@ -0,0 +1,38 @@
+//
+//  GFTitleLabel.swift
+//  GITHUB
+//
+//  Created by Briana Bayne on 1/17/24.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    // storyboard init
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        // will break it up if it's to line
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
