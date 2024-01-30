0b05c2ac712f2b68684fcb0189a16b77e59e40b2
Briana Bayne <brianabayne1@gmail.com>
2024-01-30 11:13:22 -0700

    Added files for models, Followers and User.

---
+++ GITHUB/GITHUB/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GITHUB
+//
+//  Created by Briana Bayne on 1/30/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
