5c9e7054ca50afcaba77f75ad7ec7c8adcf2564b
Briana Bayne <brianabayne1@gmail.com>
2024-02-02 13:26:31 -0700

    Images are pulling but images are large. Need to debugg.

--- GITHUB/GITHUB/CustomViews/GFAvatarImageView.swift
+++ GITHUB/GITHUB/CustomViews/GFAvatarImageView.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
                                            // using a bang here because I know I have the image in my supporting files.
-//    let placeholderImage = UIImage(named: "avatar-placeholder")!
+//   let placeholderImage = UIImage(named: "avatar-placeholder")!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        configure()
     }
     
     required init?(coder: NSCoder) {
@@ -25,5 +25,24 @@ class GFAvatarImageView: UIImageView {
         clipsToBounds = true
 //        image = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func downloadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self]  data, response, error in
+            // guarding self because it now optional since I used weak self, weak self is always optional 
+           guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        // what actually calls the URLSESSION
+        task.resume()
     }
 }
