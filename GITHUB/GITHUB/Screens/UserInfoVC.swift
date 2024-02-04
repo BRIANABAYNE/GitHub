ef87376cd173e533f5095dc7f4a1c9deb84336d2
Briana Bayne <brianabayne1@gmail.com>
2024-02-04 13:48:40 -0700

    Network call for a single user done, changed model.

--- GITHUB/GITHUB/Screens/UserInfoVC.swift
+++ GITHUB/GITHUB/Screens/UserInfoVC.swift
@@ -16,7 +16,15 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dissMissVC))
         navigationItem.rightBarButtonItem = doneButton
-        print(userName!)
+        NetworkManager.shared.getUserInfo(for: userName) { [weak self]result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(alertTitle: "Something went wrong", message: error.rawValue, buttonTitle: "OKAY")
+            }
+        }
     }
     
     @objc func dissMissVC() {
