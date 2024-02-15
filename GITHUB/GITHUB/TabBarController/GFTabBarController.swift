7fb8c8d7f3f673ba97a8d4cb11702ab7e2ec4963
Briana Bayne <brianabayne1@gmail.com>
2024-02-15 15:47:46 -0700

    Organized Files.

--- GITHUB/GITHUB/TabBarController/GFTabBarController.swift
+++ GITHUB/GITHUB/TabBarController/GFTabBarController.swift
@@ -17,23 +17,16 @@ class GFTabBarController: UITabBarController {
     
 
     func createSearchNC() -> UINavigationController {
-        // init the SearchVC
         let searchVC  = SearchVC()
-        // giving it a title
         searchVC.title = "Search"
-        // adding the system image
         searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        // returning a navigationController that we just created
         return UINavigationController(rootViewController: searchVC)
         
     }
     
     func createFavoritesNC() -> UINavigationController {
-        // init the FavoriteListVC
         let favoriteListVC = FavoriteListVC()
-        // Give it a title
         favoriteListVC.title = "Favorites"
-        // tabbar and
         favoriteListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
         return UINavigationController(rootViewController: favoriteListVC)
         
